57ca00fc79509645b50b4b1455f54365e0b2ef75
Jessica Engel <Jessicaepdx@gmail.com>
2015-07-02 11:19:01 -0700

    add project to skill, redirects to skill page displaying project name

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,6 +5,15 @@ def new
   @project = @skill.projects.new()
 end
 
+def create
+  @skill = Skill.find(params[:skill_id])
+  @project = @skill.projects.new(project_params)
+  if @project.save
+    redirect_to skill_path(@skill)
+  else
+    render :new
+  end
+end
 private
   def project_params
     params.require(:project).permit(:name, :description, :url)
