49436d7894b4fdbf24d55ff04449dc9d6f2c1534
Jessica Engel <Jessicaepdx@gmail.com>
2015-07-02 10:19:54 -0700

    add skill with add skill form and sends you back to homepage listing skill on page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -9,4 +9,18 @@ def new
   @skill = Skill.new()
 end
 
+def create
+  @skill = Skill.new(skill_params)
+  if @skill.save
+    redirect_to root_path
+  else
+    render :new
+  end
+end
+
+private
+  def skill_params
+    params.require(:skill).permit(:name, :description)
+  end
+
 end
