e38676588e883366d4975673eb947bf5ec232f9e
Jessica <Jessicaepdx@gmail.com>
2015-07-10 11:58:24 -0700

    admin profile shows images for github and linkedin links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :username, :name) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :name, :linkedin, :github, :description) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password, :username, :name, :linkedin, :github, :description, :image) }
   end
   # End Devise Stuff
 
