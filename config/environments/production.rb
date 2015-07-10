35cac1dc904985675a92d204652d8b75fa67f834
Jessica <Jessicaepdx@gmail.com>
2015-07-10 16:32:16 -0700

    deprecated config/production statement fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.serve_static_assets = true
+  config.serve_static_files = true
 
 end
