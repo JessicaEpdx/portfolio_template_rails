c95a8ca870120274ca4b6d166a7a14cc37c190ec
Jessica <Jessicaepdx@gmail.com>
2015-07-10 11:11:08 -0700

    sign up includes username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates_presence_of :name
   validates_presence_of :email
-  validates_presence_of :description
 
 
 end
