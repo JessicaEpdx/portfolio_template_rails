a289c9d3db116c616bad9b7d28121040ea263758
Jessica Engel <Jessicaepdx@gmail.com>
2015-08-26 21:59:47 -0700

    will have add reference form ajax loaded

--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -4,4 +4,24 @@ class ReferencesController < ApplicationController
     @references = Reference.all
   end
 
+  def new
+    @reference = Reference.new
+  end
+
+  def create
+    @references = Reference.all
+    @reference = Reference.new(reference_params)
+    if @reference.save
+      respond_to do |format|
+        format.html { redirect_to root_path}
+        format.js
+      end
+    end
+  end
+
+  private
+    def reference_params
+      params.require(:reference).permit(:name, :email, :address, :phone, :job_title, :employer)
+    end
+
 end
